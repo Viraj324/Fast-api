977f62c2c2fcbe3b5dd17c0eb771f480aa120299
Viraj <viraj.sonawane22@pccoepune.org>
2023-04-29 14:07:25 +0530

    Commit after creating blog page

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {"data " : {"name" : 'Viraj'}}
 
-@app.get('/about')
-def about():
-    return {'data' : "about page "} 
+#@app -> path operation decorator
+#get-> path operation function
+#('/') -> path 
+@app.get('/blog') 
+def index(limit = 10,published:bool = True, sort : Optional[str] = 'none'):
+    return published
+    if published:
+        return {"data " : f'{limit} published blogs from the list'}
+    else:
+        return {"data " : f'{limit} blogs from the list'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data' : 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def about(id:int):
+    return {'data' : id} 
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments of blog with id = id 
+    return {'data' : {'1','2'}}
+
+
+class Blog(BaseModel):
+    title : str
+    body : str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+      return {'data' : f"Blog is created with title as {blog.title}"}
+
+
+
+#if __name__ == "__main__":
+#    uvicorn.run(app,host='127.0.0.1',port=9000)
